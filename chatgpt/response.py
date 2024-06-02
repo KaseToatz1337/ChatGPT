ad59d76adfd785feaf18125e4bb026b86ec78c4a
KäseToatz <airfryerkipcorn@gmail.com>
2024-06-02 21:47:15 +0200

    added histories & fail handling

--- chatgpt/response.py
+++ chatgpt/response.py
@@ -1,5 +1,6 @@
 class Response:
 
-    def __init__(self, text: str, historyID: str) -> None:
+    def __init__(self, text: str, historyID: str, user: bool = False) -> None:
         self.text = text
         self.historyID = historyID
+        self.user = user
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ChatGPT",
-    version="1.0",
+    version="1.1",
     description="ChatGPT Playwright",
     author="KaseToatz1337",
     packages=["chatgpt"],
